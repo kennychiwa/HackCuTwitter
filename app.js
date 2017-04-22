00db81f0d8f4bf9b4f0ab5e882cfaddc47b277dd
Ryan Fasching <ryan.fasching@colorado.edu>
2017-04-22 12:45:40 -0600

    token and keys

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 
 var twitterpack = require('twitter');
+
+
+var struct {
+  consumer_key: 'iuXFMU69WJSaaqAe9yVvr30CF',
+  consumer_secret: 'r3uy3ZRh9oXPuBoK7Dfj3zoiZ2nUOTBWUjP4DqtGXIlzzbxbOY',
+  token_key: '	194272370-zNHmdL43N6RZJeOtJeH8ZhrursJUQlAqCycHqxbl',
+  tokenk_secre: '3j9LEcxAjUq1hLka2QYwSSZStETF7Mr2mBikHYCTTqodV'
+
+}
+
+var Twitter = new TwitterPackage(secret);

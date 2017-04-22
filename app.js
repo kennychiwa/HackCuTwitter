918b36fcb6b381d6464138702693c1ea5f7501a0
Ryan Fasching <ryan.fasching@colorado.edu>
2017-04-22 12:59:20 -0600

    tokens

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
-var twitterpack = require('twitter');
+var TwitterPackage = require('twitter');
+var TwitterStream = require('twitter-stream-api'), fs = require('fs')
 
 
-var struct {
+var tokens {
   consumer_key: 'iuXFMU69WJSaaqAe9yVvr30CF',
   consumer_secret: 'r3uy3ZRh9oXPuBoK7Dfj3zoiZ2nUOTBWUjP4DqtGXIlzzbxbOY',
   token_key: '	194272370-zNHmdL43N6RZJeOtJeH8ZhrursJUQlAqCycHqxbl',
@@ -11,3 +12,7 @@ var struct {
 }
 
 var Twitter = new TwitterPackage(secret);
+var TwitterStr = new TwitterStream(keys, false);
+TwitterStr.stream('statuses/filter', {
+  track: 'javascript'
+});
